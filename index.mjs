f57672541c1f9f7a5980f95d34b13a79fab279ee
YeEun Kim <kye1115z@naver.com>
2024-11-03 18:05:23 -0800

    fix: solving click event error

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 import express from "express";
-const planets = (await import("npm-solarsystem")).default;
-const ACCESS_KEY = "LZTg9NbqauEpUq14vj8GhthqOF3aXTcqYqf2_UfB2_s";
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   res.render("home");
@@ -13,8 +13,12 @@ app.get("/", async (req, res) => {
 
 app.post("/country", (req, res) => {
   const countryName = req.body.country;
-  console.log("Country received: ", countryName);
   res.redirect(`/country/${encodeURIComponent(countryName)}`);
+});
+
+app.get("/country/:countryName", (req, res) => {
+  const { countryName } = req.params;
+  res.render("country", { countryName });
 });
 
 app.listen(10040, () => {
