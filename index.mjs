4a405d6fbf9e6d0029d6b317ccab88086e4fb268
YeEun Kim <kye1115z@naver.com>
2024-11-03 19:28:13 -0800

    feat: country info

--- index.mjs
+++ index.mjs
@@ -12,13 +12,67 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/country", (req, res) => {
-  const countryName = req.body.country;
-  res.redirect(`/country/${encodeURIComponent(countryName)}`);
+  const countryId = req.body.country;
+  res.redirect(`/country/${encodeURIComponent(countryId)}`);
 });
 
-app.get("/country/:countryName", (req, res) => {
-  const { countryName } = req.params;
-  res.render("country", { countryName });
+app.get("/country/:countryId?", async (req, res) => {
+  const countryId = req.params.countryId;
+
+  try {
+    // get all of the countries
+    const countries = await fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) =>
+        data
+          .map((country) => ({
+            name: country.name.common,
+            cca2: country.cca2,
+          }))
+          .sort((a, b) => a.name.localeCompare(b.name))
+      );
+
+    const defaultCountry =
+      countries.find((country) => country.cca2 === countryId) ||
+      countries.find((country) => country.cca2 === "US");
+
+    const countryDetails = await fetch(
+      `https://restcountries.com/v3.1/alpha/${defaultCountry.cca2}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data || !data[0]) {
+          throw new Error("Country data is not available");
+        }
+
+        const countryData = data[0];
+
+        return {
+          name: countryData.name.common,
+          cca2: countryData.cca2,
+          capital: countryData.capital ? countryData.capital[0] : "N/A",
+          area: countryData.area,
+          population: countryData.population,
+          languages: countryData.languages
+            ? Object.values(countryData.languages).join(", ")
+            : "N/A",
+          currencies: countryData.currencies
+            ? Object.values(countryData.currencies)
+                .map((curr) => `${curr.name} (${curr.symbol})`)
+                .join(", ")
+            : "N/A",
+          maps: countryData.maps ? countryData.maps.googleMaps : "N/A",
+          flag: countryData.flags ? countryData.flags.svg : "N/A",
+        };
+      });
+
+    console.log(countryDetails);
+
+    res.render("country", { defaultCountry, countries, countryDetails });
+  } catch (error) {
+    console.error("Error fetching countries:", error);
+    res.status(500).send("Error fetching countries");
+  }
 });
 
 app.listen(10040, () => {
